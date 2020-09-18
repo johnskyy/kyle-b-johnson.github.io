9142407a80e3d5e7064a67cd23edbd3d645da6dc
kyle-b-johnson <johnson.kyleb@gmail.com>
2020-09-17 21:11:34 -0500

    added img and js date

---
+++ scripts/update-date.js
@@ -0,0 +1,11 @@
+
+    var now = new Date();
+    let monthNames =['January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'];
+    var thisMonth = monthNames[now.getMonth()-1];
+    var day = now.getDate();
+    var year = now.getFullYear();
+    var updatedDate = day.toString() + " " + thisMonth 
+                + " " + year.toString();
+
+    document.getElementById('datePlace').innerHTML = updatedDate;
