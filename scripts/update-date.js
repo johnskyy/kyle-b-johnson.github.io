45c9e2e7797947a2b218a1a054e2d8b05ce96807
kyle-b-johnson <johnson.kyleb@gmail.com>
2020-09-23 17:10:51 -0500

    update portal page

--- scripts/update-date.js
+++ scripts/update-date.js
@@ -2,7 +2,7 @@
     let now = new Date(document.lastModified);
     let monthNames =['January', 'February', 'March', 'April', 'May', 'June',
         'July', 'August', 'September', 'October', 'November', 'December'];
-    var thisMonth = monthNames[now.getMonth()-1];
+    var thisMonth = monthNames[now.getMonth()];
     var day = now.getDate();
     var year = now.getFullYear();
     var updatedDate = day.toString() + " " + thisMonth 
