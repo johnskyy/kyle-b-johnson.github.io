af07ae0f7a33ffa15546e51e76cca080f37c834b
kyle-b-johnson <johnson.kyleb@gmail.com>
2020-10-14 14:03:56 -0500

    Fixed Lesson 5 JS issues

--- lesson5/js/saturday.js
+++ lesson5/js/saturday.js
@@ -1,12 +1,10 @@
-const today = Date.now();
-const day = today.getDay();
+var today = new Date();
+var day = today.getDay();
 
 if (day == 5) {
   document
     .getElementById('info-banner')
-    .innerHTML(
-      'Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.'
-    );
-} else document.getElementById('info-banner').innerHTML('Today is ' + day);
-
+    .innerHTML =
+      "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.";
+}
 
